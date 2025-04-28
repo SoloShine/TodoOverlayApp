c1d1ca97546d59e1ad821b674f99e3774905a054
SoloShine <soloshine.chf@foxmail.com>
2025-04-29 02:58:12 +0800

    关于

--- Views/AboutWindow.xaml.cs
+++ Views/AboutWindow.xaml.cs
@@ -13,5 +13,17 @@ namespace TodoOverlayApp.Views
         {
             this.Close();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"打开链接时出错: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
