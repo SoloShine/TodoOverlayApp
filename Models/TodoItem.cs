9eb66da44be1ba3e917654a4e731eba81a355c38
SoloShine <soloshine.chf@foxmail.com>
2025-05-01 04:25:44 +0800

    重构数据结构

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -4,42 +4,113 @@ using System.ComponentModel;
 
 namespace TodoOverlayApp.Models
 {
-    public class TodoItem : INotifyPropertyChanged
+    public class TodoItem : BaseModel
     {
-        public TodoItem()
+        private string _id = string.Empty;
+        /// <summary>
+        /// 唯一标识符
+        /// </summary>
+        public string Id
         {
-            // 确保每个TodoItem都有唯一ID
-            Id = Guid.NewGuid().ToString();
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
         }
-        // 唯一标识符
-        public string Id { get; set; }
-
-        private string _content = "";
-        private bool _isCompleted;
-
+        private string _name = string.Empty;
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(Name));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            }
+        }
+        private string _description = string.Empty;
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value) { 
+                    _description = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(Description));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            }
+        }
+        private string? parentId = string.Empty;
         /// <summary>
         /// 父节点id
         /// </summary>
-        public string? ParentId { get; set; }
+        public string? ParentId 
+        { 
+            get => parentId; 
+            set 
+            {
+                if (parentId != value)
+                {
+                    parentId = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(ParentId));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            } 
+        }
 
-
+        private string _content = "";
+        /// <summary>
+        /// 内容
+        /// </summary>
         public string Content
         {
             get => _content;
             set
             {
-                _content = value;
-                OnPropertyChanged(nameof(Content));
+                if (_content != value)
+                {
+                    _content = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(Content));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
             }
         }
 
+        private bool _isCompleted;
+        /// <summary>
+        /// 是否完成
+        /// </summary>
         public bool IsCompleted
         {
             get => _isCompleted;
             set
             {
-                _isCompleted = value;
-                OnPropertyChanged(nameof(IsCompleted));
+                if (_isCompleted != value)
+                {
+                    _isCompleted = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(IsCompleted));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
             }
         }
 
@@ -52,45 +123,122 @@ namespace TodoOverlayApp.Models
             get => isExpanded;
             set
             {
-                isExpanded = value;
-                OnPropertyChanged(nameof(IsExpanded));
+                if (isExpanded != value)
+                {
+                    isExpanded = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(IsExpanded));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
             }
         }
 
-        private ObservableCollection<TodoItem> _subItems = [];
+        private string? appPath = string.Empty;
         /// <summary>
-        /// 子待办项集合
+        /// 应用路径
         /// </summary>
-        public ObservableCollection<TodoItem> SubItems
+        public string? AppPath
         {
-            get => _subItems;
+            get => appPath;
             set
             {
-                _subItems = value;
-                OnPropertyChanged(nameof(SubItems));
+                if (appPath != value)
+                {
+                    appPath = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(AppPath));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
             }
         }
 
-        private string? description = "";
+        private bool isInjected = false;
         /// <summary>
-        /// 描述
+        /// 是否注入软件
         /// </summary>
-        public string? Description
+        public bool IsInjected
         {
-            get => description;
+            get => isInjected;
             set
             {
-                description = value;
-                OnPropertyChanged(nameof(Description));
+                if (isInjected != value)
+                {
+                    isInjected = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(IsInjected));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
             }
         }
 
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
+        private TodoItemType todoItemType = TodoItemType.Normal;
+        /// <summary>
+        /// 待办类型
+        /// </summary>
+        public TodoItemType TodoItemType
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            get => todoItemType;
+            set
+            {
+                if (todoItemType != value)
+                {
+                    todoItemType = value;
+                    UpdatedAt = DateTime.Now;
+                    OnPropertyChanged(nameof(TodoItemType));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            }
         }
+
+        private DateTime? createdAt = DateTime.Now;
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? GreadtedAt
+        {
+            get => createdAt;
+            set
+            {
+                if (createdAt != value)
+                {
+                    createdAt = value;
+                    OnPropertyChanged(nameof(GreadtedAt));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            }
+        }
+
+        private DateTime? updatedAt = DateTime.Now;
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime? UpdatedAt
+        {
+            get => updatedAt;
+            set
+            {
+                if (updatedAt != value)
+                {
+                    updatedAt = value;
+                    OnPropertyChanged(nameof(UpdatedAt));
+                    if (!string.IsNullOrEmpty(Id)) App.TodoItemRepository.UpdateAsync(this).ConfigureAwait(false);
+                }
+            }
+        }
+
+    }
+
+    public enum TodoItemType
+    {
+        /// <summary>
+        /// 普通待办
+        /// </summary>
+        [Description("普通待办")]
+        Normal,
+        /// <summary>
+        /// 软件待办
+        /// </summary>
+        [Description("软件待办")]
+        App,
     }
 }
